560f6414991c6ebad7de696e440833afb212ed8b
Adi Harif <adi.harif@campus.technion.ac.il>
2024-06-28 20:11:47 +0300

    Adds template cpp file for vector lib emitting

---
+++ templates/vector.cpp
@@ -0,0 +1,46 @@
+
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+#ifndef ELEM_TYPE
+#error ELEM_TYPE not defined
+#endif
+#ifndef TYPE_EXT
+#error TYPE_EXT not defined
+#endif
+
+#define FUNC_EXT_I2(name, ext) name##_##ext
+#define FUNC_EXT_I1(name, ext) FUNC_EXT_I2(name, ext)
+#define FUNC_EXT(name) FUNC_EXT_I1(name, TYPE_EXT)
+
+typedef ELEM_TYPE T;
+
+extern "C" {
+
+vector<T> FUNC_EXT(create_vector)() {
+    return vector<T>();
+}
+
+vector<T> FUNC_EXT(create_sized_vector)(size_t size) {
+    return vector<T>(size);
+}
+
+void FUNC_EXT(push_back)(vector<T> &v, T value) {
+    v.push_back(value);
+}
+
+T FUNC_EXT(get)(vector<T> &v, size_t index) {
+    return v[index];
+}
+
+void FUNC_EXT(set)(vector<T> &v, size_t index, T value) {
+    v[index] = value;
+}
+
+size_t FUNC_EXT(size)(vector<T> &v) {
+    return v.size();
+}
+
+}
