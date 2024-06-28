dc8e5ac36ceae089796c680e0a13d166ac643474
Adi Harif <adi.harif@campus.technion.ac.il>
2024-06-29 01:03:14 +0300

    Simplifies names allocator

--- src/names_allocator.ts
+++ src/names_allocator.ts
@@ -8,38 +8,30 @@ import { CodeGenIterable } from './codegen_iterable.js';
 
 export function allocateNames(graph: ir.Graph): Map<ir.Vertex, NamedValue> {
     const map = new Map<ir.Vertex, NamedValue>();
-    const tmpControlMap = new Map<ir.ControlVertex, Label>();
-    let lastReg = 0;
     let lastLabel = 0;
-    const iterableGraph = new CodeGenIterable(graph);
-    let name;
-    for (let vertex of iterableGraph) {
-        if (vertex.category == ir.VertexCategory.Data || vertex.category == ir.VertexCategory.Compound) {
-            if (vertex.kind == ir.VertexKind.Parameter) {
-                name = `%${(vertex as ir.ParameterVertex).position}`;
-            }
-            else {
-                name = `%r${lastReg++}`;
-            }
-            map.set(vertex, name);
-        }
-        switch (vertex.kind) {
-            case ir.VertexKind.Start:
-            case ir.VertexKind.BlockStart:
-            case ir.VertexKind.Merge:
-                name = `l${lastLabel++}`;
-                map.set(vertex, name);
-                tmpControlMap.set(vertex as ir.ControlVertex, name);
-                break;
-            case ir.VertexKind.Branch:
-            case ir.VertexKind.Return:
-            case ir.VertexKind.BlockEnd:
-                map.set(vertex, tmpControlMap.get(vertex as ir.ControlVertex)!);
-                break;
+    for (let vertex of new CodeGenIterable(graph)) {
+        if (vertex.kind == ir.VertexKind.Start || vertex.kind == ir.VertexKind.BlockStart || vertex.kind == ir.VertexKind.Merge) {
+            let labelName = `l${lastLabel++}`;
+            map.set(vertex, labelName);
         }
         if (vertex instanceof ir.NonTerminalControlVertex) {
-            tmpControlMap.set(vertex.next!, map.get(vertex)!);
+            map.set(vertex.next!, map.get(vertex)!);
         }
-    };
+    }
+
+    let lastReg = 0;
+    for (let vertex of new CodeGenIterable(graph)) {
+        if (vertex.category == ir.VertexCategory.Data || vertex.category == ir.VertexCategory.Compound) {
+            let regName: NamedValue;
+            if (vertex.kind == ir.VertexKind.Parameter) {
+                regName = `%${(vertex as ir.ParameterVertex).position}`;
+            }
+            else {
+                regName = `%r${lastReg++}`;
+            }
+            map.set(vertex, regName);
+        }
+    }
+
     return map;
 }
