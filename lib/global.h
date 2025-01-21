3d70bb91de1ab2eb612bfbdeb54e7161d1a2828e
Adi Harif <adi.harif@campus.technion.ac.il>
2025-01-21 10:40:44 +0200

    Add implementation of a global assert function

--- lib/global.h
+++ lib/global.h
@@ -1,7 +1,14 @@
 
 #include <string>
 #include <cstdint>
+#include <stdexcept>
 
 int64_t parseInt(const std::string& s) {
     return std::stol(s);
+}
+
+void assert(bool b) {
+    if (!b) {
+        throw std::runtime_error("Assertion failed");
+    }
 }
