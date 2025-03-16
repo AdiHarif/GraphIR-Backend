f810d6f51d6878058cb14ac9fe2e5d20051f5690
Adi Harif <adi.harif@campus.technion.ac.il>
2025-03-16 10:15:39 +0200

    Add abs, sqrt and min to Math lib

--- lib/Math.h
+++ lib/Math.h
@@ -12,4 +12,29 @@ public:
             return std::floor(t);
         }
     } _floor;
+
+    class abs {
+    public:
+        template <typename T>
+        double operator()(const T& t) {
+            return std::abs(t);
+        }
+    } _abs;
+
+    class sqrt {
+    public:
+        template <typename T>
+        double operator()(const T& t) {
+            return std::sqrt(t);
+        }
+    } _sqrt;
+
+    class min {
+    public:
+        template <typename T>
+        double operator()(const T& t1, const T& t2) {
+            return std::min(t1, t2);
+        }
+    } _min;
+
 } _Math;
