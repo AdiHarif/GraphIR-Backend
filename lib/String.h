f4719794984d145315efbaf9eb4dff2a33714a44
Adi Harif <adi.harif@campus.technion.ac.il>
2025-04-02 02:49:11 +0300

    Add string comparisons with integers

--- lib/String.h
+++ lib/String.h
@@ -1,5 +1,9 @@
 
+#pragma once
+
 #include <string>
+
+#include "global.h"
 
 class String {
 public:
@@ -24,4 +28,19 @@ std::string operator+(const std::string& a, double b) {
 
 std::string operator+(double a, const std::string& b) {
     return std::to_string(a) + b;
+}
+
+bool operator<(const std::string& a, int64_t b) {
+    return parseInt(a) < b;
+}
+
+bool operator<(int64_t a, const std::string& b) {
+    return a < parseInt(b);
+}
+
+bool operator>(const std::string& a, int64_t b) {
+    return parseInt(a) > b;
+}
+bool operator>(int64_t a, const std::string& b) {
+    return a > parseInt(b);
 }
